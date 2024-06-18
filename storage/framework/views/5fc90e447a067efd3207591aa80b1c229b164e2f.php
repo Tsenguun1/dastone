3f40313be369ff32d2bdc5a7cb083aca12671b07
Batka-1216 <160466304+Batka-1216@users.noreply.github.com>
2024-06-18 09:21:57 +0800

    kk

---
+++ storage/framework/views/5fc90e447a067efd3207591aa80b1c229b164e2f.php
@@ -0,0 +1,105 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#UpdatePositionForm').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget); // Button that triggered the modal
+            var posId = button.data('id');
+            var posName = button.data('name');
+            var status = button.data('status');
+            var sortOrder = button.data('sort');
+
+            var modal = $(this);
+            modal.find('#modal-pos-id').val(posId);
+            modal.find('#posName').val(posName);
+            modal.find('#status').val(status);
+            modal.find('#sortOrder').val(sortOrder);
+        });
+    });
+</script>
+<div class="modal fade" id="AddPositionForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalDefaultLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document" style="max-width: 900px;">
+        <div class="modal-content">
+            <div class="form-container" id="formContainer">
+                <?php if(session('success')): ?>
+                    <div class="alert alert-success">
+                        <?php echo e(session('success')); ?>
+
+                    </div>
+                <?php endif; ?>
+                <?php if($errors->any()): ?>
+                    <div class="alert alert-danger">
+                        <ul>
+                            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li><?php echo e($error); ?></li>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <form id="registrationForm" method="POST" action="<?php echo e(route('addformpos')); ?>">
+                    <?php echo csrf_field(); ?>
+                    <label for="posName" class="form-check-label">Албан тушаалын нэршил</label>
+                    <input type="text" class="form-check-input" id="posName" name="posName" required>
+
+                    <label for="status" class="form-check-label">Төлөв</label>
+                    <select id="status" name="status" required>
+                        <option value="A">Идэвхитэй</option>
+                        <option value="N">Идэвхгүй</option>
+                    </select>
+
+                    <label for="sortOrder" class="form-check-label">Эрэмбэ</label>
+                    <input type="text" class="form-check-input" id="sortOrder" name="sortOrder" required>
+
+                    <button class="btn btn-primary" type="submit" name="submit">Хадгалах</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Хаах</button>
+                </form>
+            </div>
+        </div><!--end modal-content-->
+    </div><!--end modal-dialog-->
+</div><!--end modal-->
+
+<div class="modal fade" id="UpdatePositionForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalDefaultLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document" style="max-width: 900px;">
+        <div class="modal-content">
+            <div class="form-container" id="formContainer">
+                <?php if(session('success')): ?>
+                    <div class="alert alert-success">
+                        <?php echo e(session('success')); ?>
+
+                    </div>
+                <?php endif; ?>
+                <?php if($errors->any()): ?>
+                    <div class="alert alert-danger">
+                        <ul>
+                            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li><?php echo e($error); ?></li>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <form id="registrationForm" method="POST" action="<?php echo e(route('updateposition')); ?>">
+                    <?php echo csrf_field(); ?>
+                    <input type="hidden" id="modal-pos-id" name="posId">
+
+                    <label for="posName" class="form-check-label">Албан тушаалын нэршил</label>
+                    <input type="text" class="form-check-input" id="posName" name="posName" required>
+
+                    <label for="status" class="form-check-label">Төлөв</label>
+                    <select id="status" name="status" required>
+                        <option value="A">Идэвхитэй</option>
+                        <option value="N">Идэвхгүй</option>
+                    </select>
+
+                    <label for="sortOrder" class="form-check-label">Эрэмбэ</label>
+                    <input type="text" class="form-check-input" id="sortOrder" name="sortOrder" required>
+
+                    <button class="btn btn-primary" type="submit" name="submit">Хадгалах</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Хаах</button>
+                </form>
+            </div>
+        </div><!--end modal-content-->
+    </div><!--end modal-dialog-->
+</div><!--end modal-->
+<?php /**PATH C:\Users\pc\Documents\GitHub\dastone\resources\views/modal/addposition.blade.php ENDPATH**/ ?>
