c29e09bc754e7bab35cf8b03f2b945e13ee2c75b
Batka-1216 <160466304+Batka-1216@users.noreply.github.com>
2024-06-26 10:13:57 +0800

    final commit

--- storage/framework/views/5c2a93e648e13ec47596e7da313dc260dec62953.php
+++ storage/framework/views/5c2a93e648e13ec47596e7da313dc260dec62953.php
@@ -62,50 +62,58 @@
     </div>
 </div>
 
-<!-- Add Place Modal -->
-<div class="modal fade" id="AddPlaceForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalDefaultLabel"
-    aria-hidden="true">
+
+
+<div class="modal fade" id="AddPlaceForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalDefaultLabel" aria-hidden="true">
     <div class="modal-dialog" role="document" style="max-width: 900px;">
         <div class="modal-content">
             <div class="form-container" id="formContainer">
                 <form id="registrationForm" method="POST" action="<?php echo e(route('addform')); ?>">
                     <?php echo csrf_field(); ?>
-                    <label for="depName" class="form-check-label">Газар нэгжийн нэршил</label>
-                    <input type="text" class="form-check-input" id="depName" name="depName" required>
-
-                    <label for="status" class="form-check-label">Төлөв</label>
-                    <select id="status" name="status" required>
-                        <option value="">[Сонгоно уу]</option>
-                        <option value="A">Идэвхитэй</option>
-                        <option value="N">Идэвхгүй</option>
-                    </select>
-
-                    <label for="sortOrder" class="form-check-label">Эрэмбэ</label>
-                    <input type="number" class="form-check-input" id="sortOrder" name="sortOrder" required>
-
-                    <label for="parentDepId" class="form-check-label">Эцэг газар нэгж</label>
-                    <select id="parentDepId" name="parentDepId" required>
-                        <option value="">[Сонгоно уу]</option>
-                        <?php $__currentLoopData = $departments; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $department): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                            <option value="<?php echo e($department->DEP_ID); ?>"><?php echo e($department->DEP_NAME); ?></option>
-                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                    </select>
-
-                    <label for="directorEmpId" class="form-check-label">Захирал</label>
-                    <select id="directorEmpId" name="directorEmpId" required>
-                        <option value="">[Сонгоно уу]</option>
-                        <?php $__currentLoopData = $employees; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $employee): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                            <option value="<?php echo e($employee->EMP_ID); ?>"><?php echo e($employee->EMPNAME); ?> (<?php echo e($employee->DEP_NAME); ?>-<?php echo e($employee->POS_NAME); ?>)
-                            </option>
-                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                    </select>
+                    <div class="mb-3">
+                        <label for="depName" class="form-label">Газар нэгжийн нэршил</label>
+                        <input type="text" class="form-control" id="depName" name="depName" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="status" class="form-label">Төлөв</label>
+                        <select id="status" name="status" class="form-select" required>
+                            <option value="">[Сонгоно уу]</option>
+                            <option value="A">Идэвхитэй</option>
+                            <option value="N">Идэвхгүй</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="sortOrder" class="form-label">Эрэмбэ</label>
+                        <input type="number" class="form-control" id="sortOrder" name="sortOrder" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="parentDepId" class="form-label">Эцэг газар нэгж</label>
+                        <select id="parentDepId" name="parentDepId" class="form-select" required>
+                            <option value="">[Сонгоно уу]</option>
+                            <?php $__currentLoopData = $departments; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $department): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <option value="<?php echo e($department->DEP_ID); ?>"><?php echo e($department->DEP_NAME); ?></option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="directorEmpId" class="form-label">Захирал</label>
+                        <select id="directorEmpId" name="directorEmpId" class="form-select" required>
+                            <option value="">[Сонгоно уу]</option>
+                            <?php $__currentLoopData = $employees; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $employee): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <option value="<?php echo e($employee->EMP_ID); ?>"><?php echo e($employee->EMPNAME); ?> (<?php echo e($employee->DEP_NAME); ?>-<?php echo e($employee->POS_NAME); ?>)</option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </select>
+                    </div>
                     <button class="btn btn-primary" type="submit" name="submit">Хадгалах</button>
                     <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Хаах</button>
                 </form>
             </div>
-        </div><!--end modal-content-->
-    </div><!--end modal-dialog-->
-</div><!--end modal-->
+        </div>
+    </div>
+</div>
+
+<!-- Add Place Modal -->
+
 
 
 <!-- Include jQuery and Bootstrap JS -->
